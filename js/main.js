4da12208e0fef2a9b15d852a314e11830bd70de5
Rade <you@example.com>
2021-05-27 12:40:46 +0200

    update

--- js/main.js
+++ js/main.js
@@ -73,3 +73,8 @@ openOne.addEventListener('click', () => modal.classList.add('show-modal'));
 close.addEventListener('click', () => modal.classList.remove('show-modal'));
 
 window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
+
+
+
+
+
